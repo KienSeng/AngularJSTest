cc320b29b5efe63e7f9ed46ebaeb1769fe4c7288
Kien Seng <kienseng5025@gmail.com>
2017-07-04 21:44:37 +0800

    test only

--- Controller/myAppController.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', ['ngRoute']);
-app.controller('myAppController', function($scope){
-	$scope.message = 'this is my first app';
-});
-
-app.controller('pageOneController', function($scope){
-	$scope.message = 'this is my first page!!!';
-});
